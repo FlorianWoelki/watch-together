ca8acf01bacfacd217a4bed5fa8706de2a21c871
= <FlorianWoelki@gmx.de>
2018-05-16 19:35:01 +0200

    Add socket io connection

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+
+const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const io = require('socket.io').listen(server)
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+})
